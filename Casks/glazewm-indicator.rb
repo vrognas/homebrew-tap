a2a97fc9db7a40b8a77a3b5c667e82e0a6e77a85
Viktor Rognås <viktor.rognas@gmail.com>
2026-03-22 15:48:54 +0100

    feat: add glazewm-indicator cask v0.1.0

---
+++ Casks/glazewm-indicator.rb
@@ -0,0 +1,17 @@
+cask "glazewm-indicator" do
+  version "0.1.0"
+  sha256 "55da4b4ada5b1e3fd82cd48d2505c4a7cf59b4e35f831616cdd25f80dde484e5"
+
+  url "https://github.com/vrognas/GlazeWMIndicator/releases/download/v#{version}/GlazeWMIndicator-#{version}.zip"
+  name "GlazeWM Indicator"
+  desc "Native macOS menu bar indicator for GlazeWM workspaces"
+  homepage "https://github.com/vrognas/GlazeWMIndicator"
+
+  depends_on macos: ">= :ventura"
+
+  app "GlazeWM Indicator.app"
+
+  zap trash: [
+    "~/Library/Preferences/io.glzr.glazewm-indicator.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,19 @@
+# Homebrew Tap
+
+Custom Homebrew formulas and casks.
+
+## Install
+
+```sh
+brew tap vrognas/tap
+```
+
+## Casks
+
+| Cask | Description |
+|------|-------------|
+| `glazewm-indicator` | Native macOS menu bar indicator for GlazeWM workspaces |
+
+```sh
+brew install --cask vrognas/tap/glazewm-indicator
+```
