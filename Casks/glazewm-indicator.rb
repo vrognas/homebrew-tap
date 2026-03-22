9f1b84f2571a15099949c1b35bd4fa995032ee0e
Viktor Rognås <viktor.rognas@gmail.com>
2026-03-22 18:10:55 +0100

    fix: update URLs after repo rename

--- Casks/glazewm-indicator.rb
+++ Casks/glazewm-indicator.rb
@@ -2,10 +2,10 @@ cask "glazewm-indicator" do
   version "0.1.1"
   sha256 "42a19848d269713f59deb417eee4c1b97ef6192931458a1ebd269c98ee331319"
 
-  url "https://github.com/vrognas/GlazeWMIndicator/releases/download/v#{version}/GlazeWMIndicator-#{version}.zip"
+  url "https://github.com/vrognas/glazewm-indicator/releases/download/v#{version}/GlazeWMIndicator-#{version}.zip"
   name "GlazeWM Indicator"
   desc "Native macOS menu bar indicator for GlazeWM workspaces"
-  homepage "https://github.com/vrognas/GlazeWMIndicator"
+  homepage "https://github.com/vrognas/glazewm-indicator"
 
   depends_on macos: ">= :ventura"
 
