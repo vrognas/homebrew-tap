de47e517baf5648b2cc5f2ba41b40fd420b2a492
Viktor Rognås <viktor.rognas@gmail.com>
2026-03-22 18:13:55 +0100

    fix: update zip filename to glazewm-indicator

--- Casks/glazewm-indicator.rb
+++ Casks/glazewm-indicator.rb
@@ -2,7 +2,7 @@ cask "glazewm-indicator" do
   version "0.1.1"
   sha256 "42a19848d269713f59deb417eee4c1b97ef6192931458a1ebd269c98ee331319"
 
-  url "https://github.com/vrognas/glazewm-indicator/releases/download/v#{version}/GlazeWMIndicator-#{version}.zip"
+  url "https://github.com/vrognas/glazewm-indicator/releases/download/v#{version}/glazewm-indicator-#{version}.zip"
   name "GlazeWM Indicator"
   desc "Native macOS menu bar indicator for GlazeWM workspaces"
   homepage "https://github.com/vrognas/glazewm-indicator"
